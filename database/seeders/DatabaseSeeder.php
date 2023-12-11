c12150720ec64c458e2f0da72ec3cc95602b6e37
zhaider113 <zhaider113@gmail.com>
2023-12-11 12:46:46 +0500

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(UserSeeder::class); 
-        // $this->call(CategorySeeder::class);
+        $this->call(UserSeeder::class); 
+        $this->call(CategorySeeder::class);
         $this->call(FoodCategorySeeder::class);
         $this->call(FoodCourseSeeder::class);
     }
